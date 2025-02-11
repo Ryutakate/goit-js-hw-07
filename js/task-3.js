675700fb2a201b3712406659496d64a112bd3b53
Ryutakate <magegavladislav@gmail.com>
2025-02-11 19:45:43 +0200

    add index.html and js file

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", (event) => {
+    const inputText = event.target.value.trim();
+    nameOutput.textContent = inputText || "Anonymous"; 
+});
